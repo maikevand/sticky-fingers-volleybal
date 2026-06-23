170401af986347a13251bce40bfab0ee477b6b6b
maike1983 <Maikevandartel@outlook.com>
2026-06-23 11:48:48 +0200

    Formulier toegevoegd zonder functionaliteiten of opmaak

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,5 +1,6 @@
 import "./Contact.css"
 import PageLayout from "../../components/page-layout/PageLayout.jsx";
+import Button from "../../components/button/Button.jsx"
 
 function Contact() {
     return (
@@ -8,6 +9,30 @@ function Contact() {
             <p>Heb je vragen of wil je vrijblijvend een keer meespelen?
                 Je kunt contact met ons opnemen via onderstaand formulier.
             </p>
+            <form className="contact-form">
+                <label for="name-field">
+                    Naam
+                    <input type="text" id="name-field" name="name" placeholder="Naam" />
+                </label>
+                <label for="email-field">
+                    E-mail
+                    <input type="email" id="email-field" name="email" placeholder="E-mail" />
+                </label>
+                <label for="telephone-field">
+                    Telefoonnummer
+                    <input type="tel" id="telephone-field" name="telephone" placeholder="Telefoonnummer" />
+                </label>
+                <label for="message-field">
+                    Bericht
+                    <textarea name="message" id="message-field" cols="30" rows="10" placeholder="Bericht"></textarea>
+                </label>
+                <Button
+                type="submit"
+                className="submit-button"
+                onClick="submit"
+                text="Verzenden"
+                />
+            </form>
         </PageLayout>
     );
 }
