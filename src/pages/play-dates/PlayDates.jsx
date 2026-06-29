f6d54795cf548561a8c543bccc4182520ec23831
maike1983 <Maikevandartel@outlook.com>
2026-06-29 09:12:33 +0200

    Speeldata lege pagina aangemaakt

---
+++ src/pages/play-dates/PlayDates.jsx
@@ -0,0 +1,12 @@
+import "./PlayDates.css";
+import PageLayout from "../../components/page-layout/PageLayout.jsx";
+
+function PlayDates() {
+    return (
+        <PageLayout className="play-dates-page">
+        <h1>Hier komen de speeldata</h1>
+        </PageLayout>
+    );
+}
+
+export default PlayDates;
