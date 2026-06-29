09d764ac29008d0d697a8b8f62fb52994943a66d
maike1983 <Maikevandartel@outlook.com>
2026-06-29 10:38:12 +0200

    Speeldata tabel met opmaak

--- src/pages/play-dates/PlayDates.jsx
+++ src/pages/play-dates/PlayDates.jsx
@@ -4,7 +4,30 @@ import PageLayout from "../../components/page-layout/PageLayout.jsx";
 function PlayDates() {
     return (
         <PageLayout className="play-dates-page">
-        <h1>Hier komen de speeldata</h1>
+            <h1>Speeldata</h1>
+            <p>📍 Gymzaal Eekbrouwersweg 2</p>
+            <p>🕗 20:00-21:30</p>
+            <table>
+                <thead>
+                <tr>
+                    <th>Datum</th>
+                    <th>Aanwezig?</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>10-07-'26</td>
+                    <td>
+                        <div className="attendance-options">
+                        <button className="attendance-selected" type="button">Ja</button>
+                        <button className="attendance-inactive" type="button">Nee</button>
+                        <button className="attendance-inactive" type="button">?</button>
+                        </div>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+
         </PageLayout>
     );
 }
