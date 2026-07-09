996ef85d9e8f6bb1a40bd76c2d023fb72e6f68e7
maike1983 <Maikevandartel@outlook.com>
2026-07-09 14:33:22 +0200

    Speeldata totalen tellen lokaal op, nog zonder koppeling backend

--- src/pages/play-dates/PlayDates.jsx
+++ src/pages/play-dates/PlayDates.jsx
@@ -2,33 +2,15 @@ import "./PlayDates.css";
 import PageLayout from "../../components/page-layout/PageLayout.jsx";
 import {useState} from "react";
 
-// const playDates = [
-//     {
-//         id: 1,
-//         date: "10-07-2026",
-//         total: 5,
-//     },
-//     {
-//         id: 2,
-//         date: "17-07-2026",
-//         total: 3,
-//     },
-//     {
-//         id: 3,
-//         date: "24-07-2026",
-//         total: 0,
-//     },
-//     {
-//         id: 4,
-//         date: "31-07-2026",
-//         total: 4,
-//     },
-// ];
-
 function PlayDates() {
     const [attendance, setAttendance] = useState({});
     const today = new Date();
     const currentDay = today.getDay();
+    const [totalAttendance, setTotalAttendance] = useState({
+        1: 0,
+        2: 0,
+        3: 0,
+    });
 
     let daysUntilFriday;
 
@@ -57,61 +39,70 @@ function PlayDates() {
     });
 
     function handleAttendanceChange(playDateId, value) {
+        const previousChoice = attendance[playDateId];
+
         setAttendance({
             ...attendance,
             [playDateId]: value,
+        });
+
+        if (previousChoice !== "yes" && value === "yes") {
+            setTotalAttendance({
+                ...totalAttendance,
+                [playDateId]: totalAttendance[playDateId] + 1,
             });
+        }
+
+        if (previousChoice === "yes" && value === "no") {
+            setTotalAttendance({
+                ...totalAttendance,
+                [playDateId]: totalAttendance[playDateId] - 1,
+            });
+        }
     }
 
-    return (
-        <PageLayout className="play-dates-page">
-            <h1>Speeldata</h1>
-            <p>📍 Gymzaal Eekbrouwersweg 2</p>
-            <p>🕗 20:00-21:30</p>
-            <table className="play-dates-table">
-                <thead>
-                <tr>
-                    <th>Datum</th>
-                    <th>Aanwezig?</th>
-                    <th>Totaal</th>
+return (
+    <PageLayout className="play-dates-page">
+        <h1>Speeldata</h1>
+        <p>📍 Gymzaal Eekbrouwersweg 2</p>
+        <p>🕗 20:00-21:30</p>
+        <table className="play-dates-table">
+            <thead>
+            <tr>
+                <th>Datum</th>
+                <th>Aanwezig?</th>
+                <th>Totaal</th>
+            </tr>
+            </thead>
+            <tbody>
+            {playDates.map((playDate) => (
+                <tr key={playDate.id}>
+                    <td>{playDate.date}</td>
+                    <td>
+                        <div className="attendance-options">
+                            <button
+                                className={attendance[playDate.id] === "yes" ? "attendance-selected" : "attendance-inactive"}
+                                type="button"
+                                onClick={() => handleAttendanceChange(playDate.id, "yes")}
+                            >
+                                Ja
+                            </button>
+                            <button
+                                className={attendance[playDate.id] === "no" ? "attendance-selected" : "attendance-inactive"}
+                                type="button"
+                                onClick={() => handleAttendanceChange(playDate.id, "no")}
+                            >
+                                Nee
+                            </button>
+                        </div>
+                    </td>
+                    <td>{totalAttendance[playDate.id]}</td>
                 </tr>
-                </thead>
-                <tbody>
-                {playDates.map((playDate) => (
-                    <tr key={playDate.id}>
-                        <td>{playDate.date}</td>
-                        <td>
-                            <div className="attendance-options">
-                                <button
-                                    className={attendance[playDate.id] === "yes" ? "attendance-selected" : "attendance-inactive"}
-                                    type="button"
-                                    onClick={() => handleAttendanceChange(playDate.id, "yes")}
-                                    >
-                                    Ja
-                                </button>
-                                <button
-                                    className={attendance[playDate.id] === "no" ? "attendance-selected" : "attendance-inactive"}
-                                    type="button"
-                                    onClick={() => handleAttendanceChange(playDate.id, "no")}
-                                >
-                                    Nee
-                                </button>
-                                <button
-                                    className={attendance[playDate.id] === "maybe" ? "attendance-selected" : "attendance-inactive"}
-                                    type="button"
-                                    onClick={() => handleAttendanceChange(playDate.id, "maybe")}
-                                >
-                                    ?
-                                </button>
-                            </div>
-                        </td>
-                        <td>{playDate.total}</td>
-                    </tr>
-                    ))}
-                </tbody>
-            </table>
-        </PageLayout>
-    );
+            ))}
+            </tbody>
+        </table>
+    </PageLayout>
+);
 }
 
 export default PlayDates;
